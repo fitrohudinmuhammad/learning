d9dca2abd8fc3cc3da9e2d39fddcb81a81ae0a18
fitrohudin <fitrohudin@portcities.net>
2025-07-17 15:37:28 +0700

    [IMP] upload

--- iv_report_faktur/__manifest__.py
+++ iv_report_faktur/__manifest__.py
@@ -6,7 +6,6 @@
     'author': 'Indivara Group',
     'maintainer': 'Indivara Group',
     'website': 'https://www.indivaragroup.com/',
-    'license': 'LGPL-3',
     'description': """
         * 2025-02-03
             - Add new wizard to generate report faktur
